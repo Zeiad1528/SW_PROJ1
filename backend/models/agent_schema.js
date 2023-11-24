aab239e35dc87170ae8866c890bb25bf572d2440
Farah-Tarek <134189708+Farah-Tarek@users.noreply.github.com>
2023-11-24 19:16:06 +0200

    Add files via upload

---
+++ backend/models/agent_schema.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema
+
+const AgentSchema = new Schema({
+
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+
+    firstname: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      select: 'firstname',
+      required: true
+    },
+    Lastname: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      select: 'Lastname',
+      required: true
+    },
+
+    userpassword: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      select: 'userpassword',
+      required: true
+    },
+    email: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      select: 'email',
+      required: true
+
+    },
+      rating:
+      {
+        type:Number,
+        enum:[1,2,3,4,5]
+      },
+      available: {
+        type: Boolean,
+        default: true
+      },
+      major:{
+        enum:['Software','Hardware','Network'],
+        type:Number
+      }
+})
+
+const SupportAgent = mongoose.model('SupportAgent', AgentSchema);
+module.exports = SupportAgent;
---
+++ backend/models/userSchema.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema
+
+const userSchema = new Schema({
+    firstname: {
+        type:String,
+        required: true
+      },
+      Lastname: {
+        type:String,
+        required: true,
+      },
+      userid: {
+        type:Number,
+        required: true,
+        unique:true
+
+      },
+      userpassword: {
+        type:Number,
+        required: true
+      },
+      email: {
+        type:String,
+        required: true,
+        unique:true
+
+      },
+     
+    
+      role: {
+        type:String,
+        required: true
+        ,enum:['user','admin','supportagent','manager']
+
+      },
+})
+const User= mongoose.model('User',userSchema);
+module.exports = User;
